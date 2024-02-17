b0abae3c34a74d3ea2228dd8926350767449b221
10thcode <attabadukwabena@gmail.com>
2024-02-17 05:00:39 +0000

    Update session_auth.py
    create a session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,26 @@
 Defines SessionAuth class
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Session Authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session
+        """
+        if user_id is None:
+            return None
+
+        if not type(user_id) is str:
+            return None
+
+        sid = uuid.uuid4()
+        self.user_id_by_session_id[sid] = user_id
+
+        return sid
