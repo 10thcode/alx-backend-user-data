532f2ddcedc61b0e4fa1cf8da7ebda694b759579
10thcode <attabadukwabena@gmail.com>
2024-02-18 05:32:14 +0000

    Add support for session db auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -25,9 +25,14 @@ if getenv("AUTH_TYPE") == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
-if getenv("AUTH_TYPE") == "session_exp_auth":
+if getenv("auth_type") == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth 
     auth = SessionExpAuth()
+
+if getenv("auth_type") == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth 
+    auth = SessionDBAuth
+
 
 
 @app.before_request
