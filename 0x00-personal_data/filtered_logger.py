ffc43c814bff2fa511738243134996b0dfd3a523
10thcode <attabadukwabena@gmail.com>
2024-03-27 16:17:00 +0000

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,31 @@ defines filter_datum function
 """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Constructor
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self._fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Filter values in incoming log records.
+        """
+        record.msg = filter_datum(self._fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return (super().format(record))
 
 
 def filter_datum(fields: List[str],
